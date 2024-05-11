bef448ceb7c52ef49556e7cd00ad365aebac4d6c
rinorocks8 <rinorocks@gmail.com>
2024-05-11 08:21:56 -0400

    fade in

--- src/app/background/shader/frag.glsl
+++ src/app/background/shader/frag.glsl
@@ -138,6 +138,7 @@ void main() {
 
     // bg, dots, mask
     vec3 finalColor = mix(waves, waves*1.5, mask);
+    finalColor = clamp(u_time, 0.0, 1.0) * finalColor;
 
     gl_FragColor.rgba = vec4(finalColor, 1.0);
 }
